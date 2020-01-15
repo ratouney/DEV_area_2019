f3ea46381020bb203c2c59151906fcc09ef0722d
Cavalli Timothée <timothee.cavalli@epitech.eu>
2020-01-15 18:35:30 +0100

    auth

--- app/src/main/java/com/example/area/After.kt
+++ app/src/main/java/com/example/area/After.kt
@@ -2,11 +2,25 @@ package com.example.area
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import net.openid.appauth.AuthorizationException
+import net.openid.appauth.AuthorizationResponse
+
+
 
 class After : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_after)
+
+        val resp = AuthorizationResponse.fromIntent(intent)
+        val ex = AuthorizationException.fromIntent(intent)
+        if (resp != null) {
+            Log.i("Auth :", resp.accessToken!!);
+        } else {
+            Log.e("Auth :", ex.toString());
+            // authorization failed, check ex for more details
+        }
     }
 }
