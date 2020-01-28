b585ce42c08ad6b737b1177a72045b992d82b28e
Caelou <lucas.zimmer@epitech.eu>
2020-01-28 16:47:41 +0100

    [+] oauth google

--- website/area/src/main.ts
+++ website/area/src/main.ts
@@ -3,6 +3,20 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import GAuth from 'vue-google-oauth2'
+
+/**
+* You should first need to place these 2 lines of code in your APP ENTRY file, e.g. src/main.js
+*
+* import GAuth from 'vue-google-oauth2'
+* Vue.use(GAuth, {clientId: '4584XXXXXXXX-2gqknkvdjfkdfkvb8uja2k65sldsms7qo9.apps.googleusercontent.com'})
+*
+*/
+const gauthOption = {
+  clientId: '87061903784-oplbf08ai2kgc6gb8v4lolhual8go4o0.apps.googleusercontent.com',
+}
+Vue.use(GAuth, gauthOption)
+
 
 Vue.config.productionTip = false
 
---
+++ website/area/src/views/Area.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+   <h1>Test</h1>
+   <button @click="handleClickSignIn" :disabled="!isLoaded">signIn</button>
+  </div>
+ </template>
+
+ <script>
+  export default {
+   name: 'test',
+   props: [],
+   components: {
+   },
+   data () {
+    return {
+     isLoaded: false
+    }
+   },
+   computed: {
+   },
+   methods: {
+    handleClickSignIn(){
+      this.$gAuth.signIn(function (user) {
+         //on success do something
+      console.log('user', user)
+      }, function (error) {
+         //on fail do something
+      })
+    }
+  },
+  mounted(){
+    let that = this
+    let checkGauthLoad = setInterval(function(){
+      that.isLoaded = that.$gAuth.isLoaded()
+      console.log('checked', that.isLoaded)
+      if(that.isLoaded) clearInterval(checkGauthLoad)
+    }, 1000);
+  }
+}
+</script>
