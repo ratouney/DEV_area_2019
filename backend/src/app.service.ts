69d46da784952234c45b372dfa25dbf14f1e81ac
Jean Pignouf <ratouney1998@gmail.com>
2020-02-12 17:28:02 +0100

    Ajusted config forthe backend to work on the vps

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Welcome to the fucking shitshow, elso known as our shitty AREA backend. Get the postman collection that explains all this garbage here : https://app.getpostman.com/join-team?invite_code=4ae64c521a2a18008d1cf4d2888fc39f&ws=7942a905-93b7-43e2-bc7c-740afd8bdbc8';
   }
 }
