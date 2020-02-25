a30ec18a553c7ff2b16df7ca36e3efee6e5a72b4
Chloé Bourgeay <comley.soph@gmail.com>
2020-02-25 15:28:15 +0100

    -m

--- backend/src/api/key.ts
+++ backend/src/api/key.ts
@@ -1,10 +1,10 @@
 // stock key from various api
 export default {
     google: {
-        clientID: "87061903784-oplbf08ai2kgc6gb8v4lolhual8go4o0.apps.googleusercontent.com",
-        clientSecret: "BjaOWkyU6mVl-2eOkSvZYCr6",
-        APIKey: "AIzaSyDqKTS8m6ObZg9GkXuGmA3-Ow6_3qSMQHA",
-        callback: "http://localhost:8080/google/callback"
+        clientID: "602804385318-hj4udn9f6rg3u3gb5ds45tiv3amdc5vr.apps.googleusercontent.com",
+        clientSecret: "SvRITnPEuR9Jcj60gDrTaGS3",
+        APIKey: "AIzaSyC4OBf3bDwMXZ09TQSyunxswsHHi8ydp8M",
+        callback: "https://leaflighted.com"
     },
     weather: "db9eb5e2ebd63b5affc7b6cb883c0610",
     spotify: {
