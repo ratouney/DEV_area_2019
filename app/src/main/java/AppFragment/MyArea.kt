b4d366a299f96309e01823045b780209fb15bf64
Cavalli Timothée <timothee.cavalli@epitech.eu>
2020-03-01 21:25:07 +0100

    a lot of if

--- app/src/main/java/AppFragment/MyArea.kt
+++ app/src/main/java/AppFragment/MyArea.kt
@@ -2,7 +2,9 @@ package AppFragment
 
 object MyArea {
     var action: String? = null
+    var actionName: String? = null
     var actionParam: String? = null
     var reaction: String? = null
+    var reactionName: String? = null
     var reactionParam: String? = null
 }
