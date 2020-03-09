99fd38d8834fe5dd1f3b99f0f567ea097d0a982a
ratouneyVPS <maxime.de-la-fouchardiere@epitech.eu>
2020-03-09 18:55:20 +0100

    Will this commit work ?

--- backend/populate.ts
+++ backend/populate.ts
@@ -1,6 +1,8 @@
 import { createConnection, Repository } from "typeorm"
 import entities, { Reaction, Area, Action, Service } from './src/entities';
 
+require('dotenv').config()
+
 async function populateReactions(rr : Repository<Reaction>, sr : Repository<Service>) {
     const gmail = await sr.findOne({where: {name: "Gmail"}});
 
@@ -8,6 +10,16 @@ async function populateReactions(rr : Repository<Reaction>, sr : Repository<Serv
     if (entry == null) {
         const newMail = rr.create();
         newMail.name = "gmail.sendMessage";
+        newMail.arguments = ["senderMail", "destMail", "title", "text"];
+        newMail.service = gmail[0];
+        newMail.description = "Send a mail to given person with given object and given body";
+        await rr.save(newMail);
+    }
+
+    entry = await rr.findOne({where: {name: "gmail.createDraft"}})
+    if (entry == null) {
+        const newMail = rr.create();
+        newMail.name = "gmail.createDraft";
         newMail.arguments = ["senderMail", "destMail", "title", "text"];
         newMail.service = gmail[0];
         newMail.description = "Send a mail to given person with given object and given body";
@@ -26,6 +38,26 @@ async function populateReactions(rr : Repository<Reaction>, sr : Repository<Serv
         await rr.save(nextSong);
     }
 
+    entry = await rr.findOne({where: {name: "spotify.prevSong"}})
+    if (entry == null) {
+        const prevSong = rr.create();
+        prevSong.name = "spotify.prevSong";
+        prevSong.arguments = [];
+        prevSong.service = spotify;
+        prevSong.description = "Play the previous song in the queue";
+        await rr.save(prevSong);
+    }
+
+    entry = await rr.findOne({where: {name: "spotify.setVolume"}})
+    if (entry == null) {
+        const setVolume = rr.create();
+        setVolume.name = "spotify.setVolume";
+        setVolume.arguments = ["data"];
+        setVolume.service = spotify;
+        setVolume.description = "Set the volume";
+        await rr.save(setVolume);
+    }
+
     const imgur = await sr.findOne({where: {name: "Imgur"}});
 
     entry = await rr.findOne({where: {name: "imgur.changeUserBio"}})
@@ -37,6 +69,29 @@ async function populateReactions(rr : Repository<Reaction>, sr : Repository<Serv
         changeBio.description = "Change your bio";
         await rr.save(changeBio);
     }
+
+    entry = await rr.findOne({where: {name: "imgur.uploadPic"}})
+    if (entry == null) {
+        const changeBio = rr.create();
+        changeBio.name = "imgur.uploadPic";
+        changeBio.arguments = ["url", "title", "text"];
+        changeBio.service = imgur;
+        changeBio.description = "Upload a picture";
+        await rr.save(changeBio);
+    }
+
+    const sheet = await sr.findOne({where: {name: "Sheet"}});
+
+    entry = await rr.findOne({where: {name: "sheet.createSheet"}})
+    if (entry == null) {
+        const createSheet = rr.create();
+        createSheet.name = "sheet.createSheet";
+        createSheet.arguments = ["title", "name"];
+        createSheet.service = imgur;
+        createSheet.description = "Create a new spreadsheet";
+        await rr.save(createSheet);
+    }
+    
 }
 
 async function populateActions(ar : Repository<Action>, sr : Repository<Service>) {
@@ -82,6 +137,72 @@ async function populateActions(ar : Repository<Action>, sr : Repository<Service>
         todayPic.service = nasa;
         todayPic.description = "Get picture of the day posted by NASA";
         await ar.save(todayPic);
+    }
+
+    entry = await ar.findOne({where: {name: "nasa.marsPhoto"}})
+    if (entry == null) {
+        const todayPic = ar.create();
+        todayPic.name = "nasa.marsPhoto";
+        todayPic.arguments = [];
+        todayPic.service = nasa;
+        todayPic.description = "Mars photo taken by rover";
+        await ar.save(todayPic);
+    }
+
+    const poke = await sr.findOne({name: "Pokemon"});
+
+    entry = await ar.findOne({where: {name: "pokemon.getPokemonByName"}})
+    if (entry == null) {
+        const todayPic = ar.create();
+        todayPic.name = "pokemon.getPokemonByName";
+        todayPic.arguments = [];
+        todayPic.service = poke;
+        todayPic.description = "Get a random pokemon info";
+        await ar.save(todayPic);
+    }
+
+    const gmail = await sr.findOne({where: {name: "Gmail"}});
+    entry = await ar.findOne({where: {name: "gmail.hasGotNewMsg"}})
+    if (entry == null) {
+        const hasGotNewMail = ar.create();
+        hasGotNewMail.name = "gmail.hasGotNewMsg";
+        hasGotNewMail.arguments = [];
+        hasGotNewMail.service = gmail;
+        hasGotNewMail.description = "Check if the user has received a new mail";
+        await ar.save(hasGotNewMail);
+    }
+
+    const sheet = await sr.findOne({name: "Sheet"});
+    entry = await ar.findOne({where: {name: "sheet.sheetChange"}})
+    if (entry == null) {
+        const sheetUpdated = ar.create();
+        sheetUpdated.name = "sheet.sheetChange";
+        sheetUpdated.arguments = ["id"];
+        sheetUpdated.service = sheet;
+        sheetUpdated.description = "Check if a sheet has been updated";
+        await ar.save(sheetUpdated);
+    }
+
+    const weather = await sr.findOne({name: "Weather"});
+
+    entry = await ar.findOne({where: {name: "weather.weatherByCity"}})
+    if (entry == null) {
+        const cityWeather = ar.create();
+        cityWeather.name = "weather.weatherByCity";
+        cityWeather.arguments = ["name"];
+        cityWeather.service = weather;
+        cityWeather.description = "Get a city's weather";
+        await ar.save(cityWeather);
+    }
+
+    entry = await ar.findOne({where: {name: "weather.uvLimitReached"}})
+    if (entry == null) {
+        const uvLimitReached = ar.create();
+        uvLimitReached.name = "weather.uvLimitReached";
+        uvLimitReached.arguments = ["name", "data"];
+        uvLimitReached.service = weather;
+        uvLimitReached.description = "Get notified if a certain UV threshhold is reached";
+        await ar.save(uvLimitReached);
     }
 }
 
@@ -143,11 +264,11 @@ async function populateServices(repo : Repository<Service>) {
 async function populate() {
     var _conn = await createConnection({
         "type": "postgres",
-        "host": process.env.DB_HOST,
+        "host": "localhost",
         "port": 5432,
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASS,
-        "database": process.env.DB_NAME,
+        "username": "root",
+        "password": "root",
+        "database": "area",
         "entities": entities,
         "name": "quickupdate",
         "logging": true
