259cc9a4ce1bd29b9b99e4f3d1b03658294e01fc
Jean Pignouf <ratouney1998@gmail.com>
2020-03-23 17:40:32 +0100

    j'en ai marre

--- website/vue.config.js
+++ website/vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     // options...
     devServer: {
-	proxy: 'http://51.75.69.196:3001/',
+    //proxy: 'http://51.75.69.196:3001/',
+    proxy: 'http://localhost:8080'
     }
 }
